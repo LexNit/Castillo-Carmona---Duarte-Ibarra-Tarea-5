a15de22b1bdb6444c7e0f3dbbe79364f7852c568
Martin <castillocarmona01@gmail.com>
2021-05-08 19:06:19 -0500

    Tarea5

---
+++ src/app/folder/folder.module.ts
@@ -0,0 +1,22 @@
+import { ComponentsModule } from './../components/components.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { FolderPageRoutingModule } from './folder-routing.module';
+
+import { FolderPage } from './folder.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    FolderPageRoutingModule,
+    ComponentsModule
+  ],
+  declarations: [FolderPage]
+})
+export class FolderPageModule {}
